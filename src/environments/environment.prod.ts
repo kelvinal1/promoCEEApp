5288ba5ede7d97d6f7b01515782d8dcc476e93f8
Javier Vazquez <vazquezjavier079@gmail.com>
2023-07-11 10:01:21 -0500

    change to production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   apiUrl: 'https://apicorpdesarrollo.curbe.com.ec/',
   apiMail: 'https://ebimail.curbe.com.ec/',
   apiCompany: 'https://ebimailcompany.dev.curbe.com.ec/',
-  token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJBRE1JTklTVFJBRE9SIiwiaWRVc3VhcmlvIjoiMSIsImV4cCI6MTY4NzQ2NTcyMSwiaXNzIjoiZG9taW5pby1mcm9udC5jb20iLCJhdWQiOiJkb21pbmlvLWJhY2suY29tIn0.qHU5c3ckLn-yRc6MuUuUo2VxdmYvc8C69gpz9801T5s'
+  token:'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJBRE1JTklTVFJBRE9SIiwiaWRVc3VhcmlvIjoiMSIsImV4cCI6MTY5MDU1MTgyMCwiaXNzIjoiZG9taW5pby1mcm9udC5jb20iLCJhdWQiOiJkb21pbmlvLWJhY2suY29tIn0.IBn9xzF051ITJ1dMEoIsa9ax5s987cB7MR9jUmIJXlM'
 
 };
